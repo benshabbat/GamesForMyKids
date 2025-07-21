2ed461cf66c40a1abb51a8c111e830dee5741949
benshabbat <benshabbat27@gmail.com>
2025-07-22 00:12:28 +0300

    feat: update Hebrew icons for letters and alef with new designs and improved SVG properties

--- gamesformykids/public/icons/HebrewIcons.tsx
+++ gamesformykids/public/icons/HebrewIcons.tsx
@@ -5,33 +5,22 @@ interface HebrewIconProps {
 
 // אייקון המציג את האות א׳ בעברית
 export const HebrewAlefIcon = ({ size = 24, className = "" }: HebrewIconProps) => (
-  <svg width={size} height={size} viewBox="0 0 24 24" className={className} fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg width={size} height={size} viewBox="0 0 24 24" className={className} fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    {/* האות א בעברית */}
     <path 
-      d="M12.5 4L9 20M12.5 4L16 20M9 14H16" 
-      stroke="currentColor" 
-      strokeWidth="2" 
-      strokeLinecap="round" 
-      strokeLinejoin="round"
+      d="M8 6 C8 6 9 8 10 10 L12 8 C12 8 13 6 15 6 C15 6 15 8 14 10 L16 12 C16 12 18 14 18 16 C18 16 16 16 14 14 L12 16 C12 16 10 18 8 18 C8 18 8 16 10 14 L8 12 C8 12 6 10 6 8 C6 8 7 6 8 6 Z M10 10 L14 10"
+      fillRule="evenodd"
     />
   </svg>
 );
 
-// אייקון המשלב את האותיות א׳ ב׳
+// אייקון שמציג את האות א׳ בעברית בצורה גרפית
 export const HebrewLettersIcon = ({ size = 24, className = "" }: HebrewIconProps) => (
-  <svg width={size} height={size} viewBox="0 0 24 24" className={className} fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path 
-      d="M6 6L4.5 18M6 6L7.5 18M4.5 12H7.5" 
-      stroke="currentColor" 
-      strokeWidth="2" 
-      strokeLinecap="round" 
-      strokeLinejoin="round"
-    />
-    <path 
-      d="M14 6C16 6 18 7 18 9.5C18 12 16 13 14 13H12V18" 
-      stroke="currentColor" 
-      strokeWidth="2" 
-      strokeLinecap="round" 
-      strokeLinejoin="round"
-    />
+  <svg width={size} height={size} viewBox="0 0 24 24" className={className} fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    {/* ציור האות א בעברית */}
+    <path d="M6 18 L6 12 L8 8 L10 12 L10 18" strokeWidth="1.5" stroke="currentColor" fill="none"/>
+    <path d="M14 8 L16 8 L18 12 L18 18" strokeWidth="1.5" stroke="currentColor" fill="none"/>
+    <path d="M8 12 L18 12" strokeWidth="1.5" stroke="currentColor" fill="none"/>
+    <path d="M10 8 L14 8" strokeWidth="1.5" stroke="currentColor" fill="none"/>
   </svg>
 );
