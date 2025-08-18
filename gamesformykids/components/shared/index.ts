e70db5093a641acca840d7dba0ad32cd81671d6f
benshabbat <benshabbat27@gmail.com>
2025-08-18 11:07:56 +0300

    refactor: Remove unused components and update exports in shared index

--- gamesformykids/components/shared/index.ts
+++ gamesformykids/components/shared/index.ts
@@ -6,9 +6,9 @@
 export { default as UniversalGameNavigation } from './UniversalGameNavigation';
 
 // Main Game Components
-export { AutoGamePage } from './AutoGamePage';
 export { AutoGamePageWithContext } from './AutoGamePageWithContext';
 export { default as AutoStartScreen } from './AutoStartScreen';
+export { UltimateGamePage } from './UltimateGamePage';
 
 // Game UI Components
 export { GameCardGrid } from './GameCardGrid';
@@ -18,14 +18,20 @@ export { default as CelebrationBox } from './CelebrationBox';
 export { default as TipsBox } from './TipsBox';
 export { GameHints } from './GameHints';
 export { ProgressDisplay } from './ProgressDisplay';
+export { GameProgressDisplay } from './GameProgressDisplay';
 
 // Card Components
 export { GameCardMap } from './CardPresets';
 
 // Other Shared Components
-export { EnhancedGameWrapper } from './EnhancedGameWrapper';
-export { AchievementsDisplay } from './AchievementsDisplay';
 export { default as BaseGameCard } from './BaseGameCard';
-export { default as Breadcrumbs } from './Breadcrumbs';
 export { default as ButtonCheckAudio } from './ButtonCheckAudio';
 export { default as ColoredShapeCard } from './ColoredShapeCard';
+export { default as GameInstructions } from './GameInstructions';
+export { default as GameItem } from './GameItem';
+export { default as GameStartButton } from './GameStartButton';
+export { default as GenericBox } from './GenericBox';
+export { default as GenericStartScreen } from './GenericStartScreen';
+export { default as OptimizedImage } from './OptimizedImage';
+export { default as UnifiedCard } from './UnifiedCard';
+export { default as UnifiedHeader } from './UnifiedHeader';
