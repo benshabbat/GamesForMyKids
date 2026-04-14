79d91f30deb0c23f46093cfd7cbc2c834b0ca211
benshabbat <benshabbat27@gmail.com>
2026-04-14 13:59:16 +0300

    refactor: reposition UserProfile component in StaticHeader and update guest user display in UserProfile

--- gamesformykids/components/layout/StaticHeader.tsx
+++ gamesformykids/components/layout/StaticHeader.tsx
@@ -7,13 +7,12 @@ import { FeatureHighlights } from './header/FeatureHighlights';
 export function StaticHeader() {
   return (
     <header className="text-center py-6 md:py-12 relative overflow-hidden">
-      <div className="absolute top-3 right-3 md:top-4 md:right-4 z-20">
-        <UserProfile />
-      </div>
-
       <HeaderBackground />
 
       <div className="relative z-10">
+        <div className="flex justify-end px-4 mb-2">
+          <UserProfile />
+        </div>
         <HeaderHero />
         <FeatureHighlights />
       </div>
