c7dc42ac390c0041de99960e40b8fb1ad036e95c
benshabbat <benshabbat27@gmail.com>
2026-04-29 10:29:01 +0300

    refactor(layout): extract siteMetadata, StructuredData, HeadLinks (#143)

---
+++ gamesformykids/components/layout/StructuredData.tsx
@@ -0,0 +1,10 @@
+import { structuredData } from '@/lib/constants/siteMetadata';
+
+export default function StructuredData() {
+  return (
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
+    />
+  );
+}
